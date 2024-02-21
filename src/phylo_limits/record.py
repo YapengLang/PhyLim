a6fb25e078ba9b3a6aa44f717c9afa3e50c70a43
Yapeng Lang <u7181074@anu.edu.au>
2024-02-21 19:21:23 +1100

    NEW: a phylo_limits data class

---
+++ src/phylo_limits/record.py
@@ -0,0 +1,36 @@
+import json
+
+from cogent3.app.composable import define_app
+from cogent3.app.result import model_result
+from cogent3.app.typing import SerialisableType
+
+from phylo_limits.check_ident import check_ident
+from phylo_limits.diagnose import diagonse
+from phylo_limits.project import project
+
+
+class PhyloLimitRec:
+    """the record of phylogenetic limits"""
+    def __init__(self, model_res:model_result):
+        self.unique_id = model_res.source
+        self.model_name = model_res.name
+        self.tree = model_res.tree
+        self.psub_dict = model_res.lf.get_all_psubs()
+
+    def record(self):    
+        self.identifiability = check_ident(...)
+        self.boundary_values = diagonse(...)
+        if self.identifiability:
+            self.projection = project(...)
+        else:
+            self.projection = None 
+        
+    def to_rich_dict(self) -> dict:
+        return print("")
+
+
+@define_app
+def app_for_all_infor(model_res:model_result) -> SerialisableType:
+    rec = PhyloLimitRec(model_res=model_res)
+    rec.record()
+    return json.dumps(rec.to_rich_dict())
