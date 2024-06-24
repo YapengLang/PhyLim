c2a626662f233a60f4ecd3155aaf6a478a743e48
Yapeng Lang <u7181074@anu.edu.au>
2024-06-24 15:32:35 +1000

    MAINT: update package version

--- src/phylo_limits/__init__.py
+++ src/phylo_limits/__init__.py
@@ -1,10 +1,11 @@
 """my doctsring
 """
+
 __author__ = "Yapeng Lang"
-__copyright__ = "Copyright 2016-2021, Yapeng Lang"
+__copyright__ = "Copyright 2022-2024, Yapeng Lang"
 __credits__ = ["Yapeng Lang"]
 __license__ = "BSD"
-__version__ = "2022.3.31"  # A DATE BASED VERSION
+__version__ = "2024.6.20"  # A DATE BASED VERSION
 __maintainer__ = "Yapeng Lang"
 __email__ = "Yapeng.Lang@anu.edu.au"
 __status__ = "alpha"
