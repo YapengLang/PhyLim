3c12cffb747ccae2b9a2d6108c971d8722f3e996
Yapeng Lang <u7181074@gmail.com>
2024-09-27 13:51:27 +1000

    NEW: defined apps for psubs classification and bound check

--- src/phylo_limits/record_phylo_limits.py
+++ src/phylo_limits/record_phylo_limits.py
@@ -6,10 +6,15 @@ from cogent3.app.composable import define_app
 from cogent3.app.result import model_result
 
 from phylo_limits.__init__ import __version__
-from phylo_limits.check_boundary import ParamRules, check_boundary
+from phylo_limits.check_boundary import (
+    BoundsViolation,
+    ParamRules,
+    check_boundary,
+)
 from phylo_limits.classify_matrix import (
     DLC,
     MatrixCategory,
+    ModelMatrixCategories,
     ModelPsubs,
     classify_matrix,
 )
@@ -109,3 +114,23 @@ class check_identifiability:
         if self.details:
             return result
         return result.identifiability
+
+
+@define_app
+class check_fit_boundary:
+    """check if there are any rate params proximity to the bounds as 1e-10.
+    This value is important as two clusters of fits divided by the value.
+    """
+
+    def main(self, model_result: model_result) -> BoundsViolation:
+        params = load_param_values(model_result)
+        return check_boundary(params)
+
+
+@define_app
+class classify_model_psubs:
+    """labels all psubs in a given ModelPsubs object which has source info"""
+
+    def main(self, model_result: model_result) -> ModelMatrixCategories:
+        psubs = load_psubs(model_result)
+        return classify_matrix(psubs)
