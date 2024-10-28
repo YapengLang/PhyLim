54cdf0abd576517a7b8cfd588c12b1b9f688d4be
Yapeng Lang <u7181074@gmail.com>
2024-10-28 11:14:52 +1100

    TST: refactor test_apps

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -25,50 +25,41 @@ DATADIR = pathlib.Path(__file__).parent / "data"
 # set alignment for computing likelihood
 _algn = load_aligned_seqs(f"{DATADIR}/piqtree2/four_otu.fasta", moltype="dna")
 
+_model_res = deserialise_object(
+    f"{DATADIR}/eval_identifiability/unid_model_result.json"
+)
+
 
 def test_load_param_values():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
-    result = load_param_values(model_res)
+    result = load_param_values(_model_res)
     assert isinstance(result, ParamRules) == True
 
 
 def test_load_psubs():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
-    result = load_psubs(model_res)
+    result = load_psubs(_model_res)
     assert isinstance(result, ModelPsubs) == True
 
 
 def test_generate_record():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )  # two I
+    # two I
     rec_app = phylo_limits()  # default `strict` == F
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     assert isinstance(record, PhyloLimitRec) == True
     assert record.check.strict == False
 
 
 def test_generate_record_strict_control():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )  # two I
+    # two I
     rec_app = phylo_limits(strict=True)  # set `strict`
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     assert isinstance(record, PhyloLimitRec) == True
     assert record.check.strict == True
     assert record.is_identifiable == False
 
 
 def test_to_rich_dict_phylolimitrec():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     rec_app = phylo_limits(strict=True)
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     result = record.to_rich_dict()
     assert isinstance(result, dict) == True
     assert all(
@@ -87,58 +78,40 @@ def test_to_rich_dict_phylolimitrec():
 
 
 def test_violation_type_phylolimitrec():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     rec_app = phylo_limits()
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     assert record.violation_type == None
 
 
 def test_has_bv_phylolimitrec():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     rec_app = phylo_limits()
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     assert record.has_BV == True
 
 
 def test_to_table_phylolimitrec():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     rec_app = phylo_limits()
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     result = record.to_table()
     assert isinstance(result, Table) == True
 
 
 def test_repr_html_phylolimitrec():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     rec_app = phylo_limits()
-    record = rec_app(model_res)
+    record = rec_app(_model_res)
     result = record._repr_html_()
     assert isinstance(result, str) == True
 
 
 def test_classify_model_psubs():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     check_app = classify_model_psubs()
-    res = check_app(model_res)
+    res = check_app(_model_res)
     assert isinstance(res, ModelMatrixCategories) == True
 
 
 def test_check_fit_boundary():
-    model_res = deserialise_object(
-        f"{DATADIR}/eval_identifiability/unid_model_result.json"
-    )
     check_app = check_fit_boundary()
-    res = check_app(model_res)
+    res = check_app(_model_res)
     assert isinstance(res, BoundsViolation) == True
 
 
