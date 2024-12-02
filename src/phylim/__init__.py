5fb8be9b23e1d89a31b2aa787b2a1e4f4e0653af
Yapeng Lang <u7181074@gmail.com>
2024-12-02 14:07:38 +1100

    DEV: init mcats

--- src/phylim/__init__.py
+++ src/phylim/__init__.py
@@ -2,6 +2,7 @@
 """
 
 from phylim.apps import check_fit_boundary, classify_model_psubs
+from phylim.classify_matrix import CHAINSAW, DLC, IDENTITY, LIMIT, SYMPATHETIC
 
 
 check_fit_boundary = check_fit_boundary()
